ff8c57dfdcc5c5d4fd6a3cabed0eb7e5d30fd48d
hakim <kimoaouini20@gmail.com>
2023-04-14 02:25:01 +0200

    first commit

--- src/app/pages/pages-register/pages-register.component.ts
+++ src/app/pages/pages-register/pages-register.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-pages-register',
   templateUrl: './pages-register.component.html',
   styleUrls: ['./pages-register.component.css']
 })
-export class PagesRegisterComponent implements OnInit {
+export class PagesRegisterComponent  {
 
-  constructor() { }
+  
 
-  ngOnInit(): void {
+  myForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.myForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.maxLength(2)]], 
+      prenom:['', [Validators.required, Validators.maxLength(2)]], 
+      tel:['', [Validators.required, Validators.maxLength(2)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
   }
-
+  onSubmit() {
+    console.log(this.myForm.value);
+  }
+  
 }
